e7b898893ee5e26eec581955c69b40be95cfa0a6
Rami Ahmed Bock <rabo@netcompany.com>
2019-04-04 14:04:08 +0200

    ApplicationDbContext (Entity framework) updated with DbSet for Cities, Connections and Parcel Types (thus creating the database tables)

--- TelstarCES/TelstarCES/Data/ApplicationDbContext.cs
+++ TelstarCES/TelstarCES/Data/ApplicationDbContext.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TelstarCES.Data.Models;
 
 namespace TelstarCES.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Connection> Connections { get; set; }
+
+        public DbSet<ParcelType> ParcelTypes { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public ApplicationDbContext()
+            : base(new DbContextOptions<ApplicationDbContext>())
+        {
+
         }
     }
 }
