0f0c0b5e83eecc0c42341861c7e116540d81e5b6
medek <met.dekaj@netcompany.com>
2019-04-04 12:51:20 +0100

    Init OrderViewModal.cs

---
+++ TelstarCES/TelstarCES/Models/OrderViewModal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+//namespace TelstarCES.Models
+//{
+  //  public class OrderViewModal
+    //{
+      //  public City[] Cities { get; set; }
+
+        //public ParcelType[] Parcels { get; set; }
+
+//    }
+//}
+
+
