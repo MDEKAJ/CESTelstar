04fd8a7d329147f2578c0e363e27590374d1381f
medek <met.dekaj@netcompany.com>
2019-04-04 12:59:46 +0100

    Complete CityViewModal

---
+++ TelstarCES/TelstarCES/Models/CityViewModal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelstarCES.Data.Models;
+
+namespace TelstarCES.Models
+{
+    public class CityViewModal
+    {
+        public Dictionary<City, Connection[]> Cities { get; set; }
+
+    }
+}
