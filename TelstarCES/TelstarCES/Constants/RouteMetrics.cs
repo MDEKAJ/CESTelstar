27996c039f3e7e0b921e1053507913157a318d2a
Rami Ahmed Bock <rabo@netcompany.com>
2019-04-04 18:25:44 +0200

    RouteMetrics constants class added with fields for the heuristic bias towards Telstar routes

---
+++ TelstarCES/TelstarCES/Constants/RouteMetrics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelstarCES.Constants
+{
+    public static class RouteMetrics
+    {
+        public const float TelstarProviderBiasFactor = 0.8f;
+
+        public const float CompetitorBiasFactor = 1f;
+    }
+}
