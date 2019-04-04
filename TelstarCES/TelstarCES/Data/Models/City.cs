47f30434ef308abf0e94d7032c8b56a1b9a62f46
Tobias Olsson <too@netcompany.com>
2019-04-04 12:54:36 +0200

    Added models

---
+++ TelstarCES/TelstarCES/Data/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelstarCES.Data.Models
+{
+    public class City
+    {
+        [Key]
+        public virtual int CityId { get; set; }
+
+        [Required]
+        public virtual string CityName { get; set; }
+
+        public virtual Connection[] Connections { get; set; }
+
+    }
+}
---
+++ TelstarCES/TelstarCES/Data/Models/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelstarCES.Data.Models
+{
+    public class Connection
+    {
+        [Key]
+        public virtual int ConnectionId { get; set; }
+
+        [Required]
+        public virtual City City1 { get; set; }
+
+        [Required]
+        public virtual City City2 { get; set; }
+
+        [Required]
+        public virtual string Provider { get; set; }
+
+        [Required]
+        public virtual float Price { get; set; }
+
+        [Required]
+        public virtual int Duration { get; set; }
+    }
+}
