668dd64d868cfb48d5e2f44769515cc38611962b
Rami Ahmed Bock <rabo@netcompany.com>
2019-04-04 14:03:34 +0200

    IDataService interface implementation

---
+++ TelstarCES/TelstarCES/Services/IDataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TelstarCES.Data.Models;
+
+namespace TelstarCES.Services
+{
+    public interface IDataService
+    {
+        #region CITY
+
+        Task<City> GetCity(int cityId);
+
+        Task<City[]> GetCities();
+
+        Task<bool> AddCity(City city);
+
+        Task<bool> UpdateCity(City city);
+
+        Task<bool> DeleteCity(City city);
+
+        #endregion
+
+        #region CONNECTIONS
+
+        Task<Connection> GetConnection(int connectionId);
+
+        Task<Connection[]> GetConnections(int cityId);
+
+        Task<Connection[]> GetConnections();
+
+        Task<bool> AddConnection(Connection connection);
+
+        Task<bool> UpdateConnection(Connection connection);
+
+        Task<bool> DeleteConnection(Connection connection);
+
+        #endregion
+
+        #region
+
+        Task<ParcelType> GetParcelType(int parcelTypeId);
+
+        Task<ParcelType[]> GetParcelTypes();
+
+        #endregion
+    }
+}
