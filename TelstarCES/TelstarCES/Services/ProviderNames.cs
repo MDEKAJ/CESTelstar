3555b30d617fbed081b549c9c6544e755625e994
Rami Ahmed Bock <rabo@netcompany.com>
2019-04-04 18:24:46 +0200

    ProviderNames constants added with the abbreviated names for the different providers

---
+++ TelstarCES/TelstarCES/Services/ProviderNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelstarCES.Services
+{
+    public static class ProviderNames
+    {
+        public const string Telstar = "TL";
+
+        public const string Oceanic = "OA";
+
+        public const string EastIndia = "EATC";
+    }
+}
