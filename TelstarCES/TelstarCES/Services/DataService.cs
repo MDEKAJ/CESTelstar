bcc9443c2139c5944336c6f7c4a04f0207bb850d
Rami Ahmed Bock <rabo@netcompany.com>
2019-04-05 10:09:31 +0200

    ensured thread safety on all data service calls

--- TelstarCES/TelstarCES/Services/DataService.cs
+++ TelstarCES/TelstarCES/Services/DataService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Identity.UI.Pages.Internal.Account;
 using Microsoft.EntityFrameworkCore;
 using TelstarCES.Data;
 using TelstarCES.Data.Models;
@@ -11,6 +13,7 @@ namespace TelstarCES.Services
 {
     public class DataService : IDataService
     {
+        private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(1, 1);
         private readonly ApplicationDbContext _db;
 
         public DataService(ApplicationDbContext db)
@@ -22,29 +25,81 @@ namespace TelstarCES.Services
 
         public async Task<City> GetCity(int cityId)
         {
-            return await _db.Cities.FirstOrDefaultAsync(c => c.CityId == cityId);
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.Cities.FirstOrDefaultAsync(c => c.CityId == cityId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<City> GetCity(string name)
         {
-            return await _db.Cities.FirstOrDefaultAsync(c => string.Equals(c.CityName, name, StringComparison.CurrentCultureIgnoreCase));
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.Cities.FirstOrDefaultAsync(c =>
+                    string.Equals(c.CityName, name, StringComparison.CurrentCultureIgnoreCase));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<City[]> GetCities()
         {
-            return await _db.Cities.ToArrayAsync();
-          
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.Cities.ToArrayAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<bool> AddCity(City city)
         {
-            await _db.Cities.AddAsync(city);
-            return await _db.SaveChangesAsync() > 0;
-   
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                await _db.Cities.AddAsync(city);
+                return await _db.SaveChangesAsync() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<bool> UpdateCity(City city)
         {
+            await _semaphoreSlim.WaitAsync();
             try
             {
                 var updateCity = await _db.Cities.FirstOrDefaultAsync(c => c.CityId == city.CityId);
@@ -54,19 +109,36 @@ namespace TelstarCES.Services
                 }
 
                 _db.Entry(updateCity).CurrentValues.SetValues(city);
-                return await _db.SaveChangesAsync() > 0;            
+                return await _db.SaveChangesAsync() > 0;
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
                 return false;
             }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<bool> DeleteCity(City city)
         {
-            _db.Cities.Remove(city);
-            return await _db.SaveChangesAsync() > 0;
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                _db.Cities.Remove(city);
+                return await _db.SaveChangesAsync() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         #endregion CITY
@@ -75,21 +147,61 @@ namespace TelstarCES.Services
 
         public async Task<Connection> GetConnection(int connectionId)
         {
-            return await _db.Connections.FirstOrDefaultAsync(c => c.ConnectionId == connectionId);
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.Connections.FirstOrDefaultAsync(c => c.ConnectionId == connectionId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<Connection[]> GetConnections(int cityId)
         {
-            return await _db.Connections.Where(c => c.City1Id == cityId || c.City2Id == cityId).ToArrayAsync();
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.Connections.Where(c => c.City1Id == cityId || c.City2Id == cityId).ToArrayAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<Connection[]> GetConnections()
         {
-            return await _db.Connections.ToArrayAsync();
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.Connections.ToArrayAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<bool> AddConnection(Connection connection)
         {
+            await _semaphoreSlim.WaitAsync();
             try
             {
                 await _db.Connections.AddAsync(connection);
@@ -97,20 +209,51 @@ namespace TelstarCES.Services
             }
             catch (Exception e)
             {
+                Console.WriteLine(e.Message);
                 return false;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
             }
         }
 
         public async Task<bool> UpdateConnection(Connection connection)
         {
-            _db.Connections.Update(connection);
-            return await _db.SaveChangesAsync() > 0;
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                _db.Connections.Update(connection);
+                return await _db.SaveChangesAsync() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<bool> DeleteConnection(Connection connection)
         {
-            _db.Connections.Remove(connection);
-            return await _db.SaveChangesAsync() > 0;
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                _db.Connections.Remove(connection);
+                return await _db.SaveChangesAsync() > 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         #endregion
@@ -119,12 +262,38 @@ namespace TelstarCES.Services
 
         public async Task<ParcelType> GetParcelType(int parcelTypeId)
         {
-            return await _db.ParcelTypes.FirstOrDefaultAsync(pt => pt.ParcelTypeId == parcelTypeId);
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.ParcelTypes.FirstOrDefaultAsync(pt => pt.ParcelTypeId == parcelTypeId);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         public async Task<ParcelType[]> GetParcelTypes()
         {
-            return await _db.ParcelTypes.ToArrayAsync();
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                return await _db.ParcelTypes.ToArrayAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         #endregion
@@ -133,9 +302,22 @@ namespace TelstarCES.Services
 
         public async Task<int> AddOrder(Order order)
         {
-            await _db.Orders.AddAsync(order);
-            await _db.SaveChangesAsync();
-            return order.OrderId;
+            await _semaphoreSlim.WaitAsync();
+            try
+            {
+                await _db.Orders.AddAsync(order);
+                await _db.SaveChangesAsync();
+                return order.OrderId;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+            finally
+            {
+                _semaphoreSlim.Release();
+            }
         }
 
         #endregion
